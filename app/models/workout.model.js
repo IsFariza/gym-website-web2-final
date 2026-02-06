0ddb744f01b7a05496d1a9334d3c128742ab879f
IsFariza <fariza.ismag@gmail.com>
2026-02-07 02:17:54 +0500

    feat: add workout model

---
+++ app/models/workout.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const workoutSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: { //Leg Day or Cardio
+    type: String,
+    required: true,
+    trim: true
+  },
+  exercises: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Exercise'
+    }
+  ],
+  level: {
+    type: String,
+    enum: ['beginner', 'intermediate', 'advanced'],
+    default: 'beginner'
+  },
+  schedule: {
+    type: String
+  }
+}, { 
+  timestamps: true 
+});
+
+module.exports = mongoose.model('Workout', workoutSchema);

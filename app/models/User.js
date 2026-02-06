166a98f30dc8c38c6e085ff6a0a2668d28c475f2
riri-anvarbekova <241194@astanait.edu.kz>
2026-02-06 23:58:30 +0500

    add models/User.ja file

---
+++ app/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    phoneNumber: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);

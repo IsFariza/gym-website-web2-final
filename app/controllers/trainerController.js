c76c9105c7f099ce304bc4d4a1e4bf785c4e0462
riri-anvarbekova <241194@astanait.edu.kz>
2026-02-07 11:19:33 +0500

    add trainerController.js

---
+++ app/controllers/trainerController.js
@@ -0,0 +1,22 @@
+const Trainer = require('../models/Trainer');
+
+// GET all trainers
+exports.getTrainers = async (req, res) => {
+  try {
+    const trainers = await Trainer.find();
+    res.json(trainers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET trainer by ID
+exports.getTrainerById = async (req, res) => {
+  try {
+    const trainer = await Trainer.findById(req.params.id);
+    if (!trainer) return res.status(404).json({ message: 'Trainer not found' });
+    res.json(trainer);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
